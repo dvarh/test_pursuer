4953b7397993c3bad747be63b12d68472e6e60ff
Иван Бережной <idvarh@gmail.com>
2016-08-28 00:03:42 +0300

    add admin interface for Man instance

--- pursuer/models.py
+++ pursuer/models.py
@@ -10,9 +10,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class ManMan(models.Model):
+class Man(models.Model):
     name = models.CharField(max_length=32)
     follow_ids = models.TextField()
 
     class Meta:
         db_table = 'man_man'
+
+
+    def __unicode__(self):
+        return self.name
